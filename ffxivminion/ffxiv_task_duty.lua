09cf7891f7bd4c7fbf21386335ec375cbded78b3
aceRage <ace.savell@gmail.com>
2014-10-01 20:21:33 -0500

    beginning of duty res task, incomplete

--- ffxivminion/ffxiv_task_duty.lua
+++ ffxivminion/ffxiv_task_duty.lua
@@ -813,4 +813,59 @@ function e_deadduty:execute()
 	end
 end
 
+ffxiv_task_duty_res = inheritsFrom(ml_task)
+function ffxiv_task_duty_res.Create()
+    local newinst = inheritsFrom(ffxiv_task_duty_res)
+    
+    --ml_task members
+    newinst.valid = true
+    newinst.completed = false
+    newinst.subtask = nil
+    newinst.auxiliary = false
+    newinst.process_elements = {}
+    newinst.overwatch_elements = {}
+    
+    --ffxiv_task_movetopos members
+    newinst.name = "LT_DUTY_RES"
+    newinst.pos = 0
+	newinst.repositioned = false
+    
+    return newinst
+end
+
+function ffxiv_task_duty_res:Init()	
+    self:AddTaskCheckCEs()
+end
+
+function ffxiv_task_duty_res:task_complete_eval()
+	if (IsLoading() or ml_mesh_mgr.loadingMesh or Player.revivestate == 3 or Player.revivestate == 1 ) then
+		return false
+	end
+	
+	local pos = self.pos
+	if (not Player.alive and ControlVisible("SelectYesno")) then
+		if (PressYesNo(true)) then
+			return false
+		end
+		if (PressOK()) then
+			return false
+		end 
+	end
+	
+	if (Player.alive and not self.repositioned) then
+		GameHacks:TeleportToXYZ(pos.x, pos.y, pos.z)
+		Player:SetFacingSynced(pos.h)
+	end
+	
+	if (Player.alive and self.repositioned) then
+		return true
+	end
+	
+    return false
+end
+
+function ffxiv_task_duty_res:task_complete_execute()
+    self.completed = true
+end
+
 RegisterEventHandler("GUI.Update",ffxiv_task_duty.GUIVarUpdate)
